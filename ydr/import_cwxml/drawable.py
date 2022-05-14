69a3cf1861fcf964906a11f0d199dc29caf9e962
Colton Fox <46362482+CFox04@users.noreply.github.com>
2022-05-14 14:51:58 -0400

    .ydr import rewrite using new design pattern

---
+++ ydr/import_cwxml/drawable.py
@@ -0,0 +1,173 @@
+import bpy
+from mathutils import Vector
+
+from ...sollumz_converter import CWXMLConverter
+from ...sollumz_properties import SollumType, LODLevel, SOLLUMZ_UI_NAMES
+from ...tools.blenderhelper import create_sollumz_object, join_objects
+from ...cwxml import drawable as ydrxml
+from ...ybn.ybnimport import composite_to_obj, bound_to_obj
+from ..ydrimport import geometry_to_obj_split_by_bone, create_lights
+from ..shader_materials import create_tinted_shader_graph
+from .geometry import GeometryCWXMLConverter
+from .skeleton import SkeletonCWXMLConverter
+from .shader import ShaderCWXMLConverter
+from .light import LightCWXMLConverter
+
+
+class DrawableCWXMLConverter(CWXMLConverter[ydrxml.Drawable]):
+    """Converts Drawable CWXML objects to bpy objects."""
+    IMPORT_CWXML_FUNC = ydrxml.Drawable.from_xml_file
+
+    @property
+    def bones_cwxml(self) -> list[ydrxml.BoneItem]:
+        """Bones of this drawable. Defaults to self.cwxml.skeleton.bones if none are provided."""
+        if not self._bones_cwxml:
+            return self.cwxml.skeleton.bones
+
+        return self._bones_cwxml
+
+    def __init__(self, cwxml: ydrxml.Drawable, external_bones: list[ydrxml.BoneItem] = None):
+        super().__init__(cwxml)
+        self.materials: list[bpy.types.Material] = []
+        self.uses_external_skeleton = external_bones is not None
+        self._bones_cwxml: list[ydrxml.BoneItem] = external_bones or []
+
+    def create_bpy_object(self, name: str) -> bpy.types.Object:
+        if self.cwxml.has_skeleton():
+            skeleton_converter = SkeletonCWXMLConverter(
+                cwxml=self.cwxml.skeleton)
+
+            self.bpy_object = skeleton_converter.create_bpy_object(
+                name, self.cwxml.joints.rotation_limits)
+        else:
+            self.bpy_object = create_sollumz_object(
+                SollumType.DRAWABLE, name=name)
+
+        self.set_drawable_lod_dist()
+        self.create_all_drawable_models()
+        self.create_embedded_collisions()
+
+        if self.cwxml.lights:
+            LightCWXMLConverter(
+                self.cwxml.lights).create_bpy_object(self.bpy_object)
+
+        if self.import_operator.import_settings.join_geometries:
+            self.join_geometries()
+
+        return self.bpy_object
+
+    def set_drawable_lod_dist(self):
+        """Set the lod distance properties for this drawable to that of the drawable cwxml."""
+        self.bpy_object.drawable_properties.lod_dist_high = self.cwxml.lod_dist_high
+        self.bpy_object.drawable_properties.lod_dist_med = self.cwxml.lod_dist_med
+        self.bpy_object.drawable_properties.lod_dist_low = self.cwxml.lod_dist_low
+        self.bpy_object.drawable_properties.lod_dist_vlow = self.cwxml.lod_dist_vlow
+
+    def create_all_drawable_models(self):
+        """Create all drawable models and parent them to the drawable."""
+        self.create_materials()
+
+        for models_group, lod_level in zip(self.cwxml.drawable_model_groups, list(LODLevel)):
+            for model_cwxml in models_group:
+                model_object = self.create_drawable_model(
+                    model_cwxml, lod_level)
+                model_object.parent = self.bpy_object
+
+                if self.cwxml.has_skeleton() and not self.uses_external_skeleton:
+                    self.parent_drawable_model_bones(
+                        model_object, model_cwxml.bone_index)
+
+    def create_materials(self):
+        """Create all materials from this drawable's cwxml shader group."""
+        for shader_cwxml in self.cwxml.shader_group.shaders:
+            material = ShaderCWXMLConverter(
+                shader_cwxml, self.filepath, self.cwxml.shader_group.texture_dictionary).create_bpy_object()
+            self.materials.append(material)
+
+    def create_embedded_collisions(self):
+        """Create all collisions defined in the drawable."""
+        # TODO: Old code. Ybn import still needs rewrite.
+        for bound in self.cwxml.bounds:
+            bobj = None
+            if bound.type == "Composite":
+                bobj = composite_to_obj(
+                    bound, SOLLUMZ_UI_NAMES[SollumType.BOUND_COMPOSITE], True)
+                bobj.parent = self.bpy_object
+            else:
+                bobj = bound_to_obj(bound)
+                if bobj:
+                    bobj.parent = self.bpy_object
+
+    def create_drawable_model(self, model_cwxml: ydrxml.DrawableModelItem, lod_level: LODLevel):
+        """Create a single drawable model given its cwxml."""
+        model_object = create_sollumz_object(SollumType.DRAWABLE_MODEL)
+
+        model_object.drawable_model_properties.sollum_lod = lod_level
+        model_object.drawable_model_properties.render_mask = model_cwxml.render_mask
+        model_object.drawable_model_properties.unknown_1 = model_cwxml.unknown_1
+        model_object.drawable_model_properties.flags = model_cwxml.flags
+
+        import_settings = self.import_operator.import_settings
+        if not import_settings.join_geometries and import_settings.split_by_bone and model_cwxml.has_skin == 1:
+            # TODO: This is old code. Still need to implement split by bone in rewrite.
+            child_objs = geometry_to_obj_split_by_bone(
+                model_cwxml, self.materials, self.bones_cwxml)
+            for child_obj in child_objs:
+                child_obj.parent = model_object
+                for mat in self.materials:
+                    child_obj.data.materials.append(mat)
+                create_tinted_shader_graph(child_obj)
+        else:
+            for geometry_cwxml in model_cwxml.geometries:
+                geometry = self.create_drawable_model_geometry(geometry_cwxml)
+                geometry.parent = model_object
+
+        return model_object
+
+    def parent_drawable_model_bones(self, model_object: bpy.types.Object, bone_index: int):
+        """Set drawable model parent bone based on its bone index."""
+        armature = self.bpy_object
+
+        if armature.type != "ARMATURE":
+            return
+
+        parent_bone_name = None
+        has_bone_translation = False
+
+        if len(armature.pose.bones) > bone_index:
+            parent_bone_name = armature.pose.bones[bone_index].name
+            translation: Vector = self.bones_cwxml[bone_index].translation
+
+            if translation is not None and translation.magnitude > 0:
+                has_bone_translation = True
+
+        if parent_bone_name is not None:
+            # Preserve transforms after parenting
+            original_world_mat = model_object.matrix_world.copy()
+
+            model_object.parent_type = "BONE"
+            model_object.parent_bone = parent_bone_name
+
+            if has_bone_translation is False:
+                model_object.matrix_world = original_world_mat
+
+    def create_drawable_model_geometry(self, geometry_cwxml: ydrxml.GeometryItem):
+        """Create a geometry object for the given drawable model bpy object."""
+        geometry_converter = GeometryCWXMLConverter(
+            geometry_cwxml)
+
+        geometry_converter.create_bpy_object(
+            self.cwxml.name, self.bones_cwxml, self.materials)
+
+        if self.cwxml.has_skeleton():
+            geometry_converter.create_armature_modifier(self.bpy_object)
+
+        return geometry_converter.bpy_object
+
+    def join_geometries(self):
+        """Join all geometries for this drawable."""
+        for drawable_model in self.bpy_object.children:
+            if drawable_model.sollum_type == SollumType.DRAWABLE_MODEL:
+                geometries = [
+                    child for child in drawable_model.children if child.sollum_type == SollumType.DRAWABLE_GEOMETRY]
+                join_objects(geometries)
---
+++ ydr/import_cwxml/shader.py
@@ -0,0 +1,178 @@
+import os
+import bpy
+from typing import Union
+
+from ...cwxml import drawable as ydrxml
+from ...cwxml.shader import ShaderManager
+from ...sollumz_converter import CWXMLConverter
+from ...sollumz_properties import MaterialType, TextureFormat, TextureUsage
+from .. import shader_materials as shader_nodes
+from ...tools.utils import get_file_name
+
+
+class ShaderCWXMLConverter(CWXMLConverter[ydrxml.ShaderItem]):
+    """Coverts CWXML shaders into bpy materials."""
+
+    @property
+    def texture_folder(self):
+        """Texture folder path from the filepath of this shader group's drawable."""
+        if self._texture_folder is not None:
+            return self._texture_folder
+
+        file_name = get_file_name(self.filepath)
+        texture_folder = f"{os.path.dirname(self.filepath)}\\{file_name}"
+
+        if os.path.exists(texture_folder):
+            self._texture_folder = texture_folder
+            return texture_folder
+
+    def __init__(
+        self, cwxml: ydrxml.ShaderItem, filepath: str,
+        texture_dictionary: list[ydrxml.TextureItem]
+    ):
+        self.bpy_object: bpy.types.Material
+
+        super().__init__(cwxml)
+
+        self.filepath = filepath
+        self.texture_dictionary = texture_dictionary
+        self._texture_folder: Union[str, None] = None
+
+    def create_bpy_object(self) -> bpy.types.Material:
+        self.bpy_object = bpy.data.materials.new(self.cwxml.name)
+        self.set_material_properties()
+        self.create_shader_nodes()
+
+        # TODO: Refactor shader_materials.py and include whats done in this loop. Everything
+        # related to nodes should be done in shader_materials.py
+        param: ydrxml.ShaderParameter
+        for param in self.cwxml.parameters:
+            for node in self.bpy_object.node_tree.nodes:
+                if isinstance(node, bpy.types.ShaderNodeTexImage) and param.name == node.name:
+                    self.load_image_texture(node, param)
+                    self.set_embedded_texture_properties(node, param)
+                elif isinstance(node, bpy.types.ShaderNodeValue):
+                    # Check if param name is equal to the node name without the _xyz suffix
+                    if param.name == node.name[:-2]:
+                        ShaderCWXMLConverter.set_value_node_properties(
+                            node, param)
+
+        self.assign_extra_detail_node()
+
+        return self.bpy_object
+
+    def set_material_properties(self):
+        """Set the properties of this shader to that of the cwxml shader."""
+        material = self.bpy_object
+        shader = self.cwxml
+
+        material.use_nodes = True
+        material.sollum_type = MaterialType.SHADER
+
+        material.shader_properties.name = shader.name
+        material.shader_properties.filename = shader.filename
+        material.shader_properties.renderbucket = shader.render_bucket
+
+    def create_shader_nodes(self):
+        """Creates the shader nodes for this material."""
+        material = self.bpy_object
+        filename = self.cwxml.filename
+
+        if filename in ShaderManager.terrains:
+            shader_nodes.create_terrain_shader(material, self.cwxml, filename)
+        else:
+            shader_nodes.create_basic_shader_nodes(
+                material, self.cwxml, filename)
+
+        shader_nodes.organize_node_tree(material.node_tree)
+
+    def load_image_texture(self, node: bpy.types.ShaderNodeTexImage, param: ydrxml.TextureShaderParameter):
+        """Load image texture for node based on the cwxml texture shader parameter given."""
+        # For some reason texture parameters will sometimes have no name.
+        if not param.texture_name:
+            return
+
+        image = bpy.data.images.get(param.texture_name)
+
+        if image is None:
+            image = self.load_image(param.texture_name)
+        if image is None:
+            image = ShaderCWXMLConverter.create_blank_image(param.texture_name)
+
+        node.image = image
+
+        # Normal maps need non-color colorspace
+        # https://docs.blender.org/manual/en/latest/render/color_management.html#image-color-spaces
+        if "Bump" in param.name:
+            node.image.colorspace_settings.name = "Non-Color"
+
+    def load_image(self, texture_name: str) -> Union[bpy.types.Image, None]:
+        """Attempt to load a .dds image with texture_name from this shader group's texture
+        folder. If a .dds file is found a bpy image will be returned, otherwise None will be
+        returned."""
+        if not self.texture_folder:
+            return None
+
+        texture_path = os.path.join(
+            self.texture_folder, texture_name + ".dds")
+
+        if os.path.isfile(texture_path):
+            bpy_image = bpy.data.images.load(
+                texture_path, check_existing=True)
+            bpy_image.name = texture_name
+            return bpy_image
+
+        return None
+
+    def set_embedded_texture_properties(self, node: bpy.types.ShaderNodeTexImage, param: ydrxml.TextureShaderParameter):
+        """Set embedded texture properties for node based on self.texture_dictionary."""
+        for texture in self.texture_dictionary:
+            if texture.name == param.texture_name:
+                node.texture_properties.embedded = True
+
+                texture_format = texture.format.replace("D3DFMT_", "")
+                if hasattr(TextureFormat, texture_format):
+                    node.texture_properties.format = TextureFormat[texture_format]
+                else:
+                    self.import_operator.report(
+                        {"WARNING"}, f"Failed to set texture format for texture '{texture.name}': format '{texture.format}' unknown.")
+
+                if hasattr(TextureUsage, texture.usage):
+                    node.texture_properties.usage = TextureUsage[texture.usage]
+                else:
+                    self.import_operator.report(
+                        {"WARNING"}, f"Failed to set texture usage for texture '{texture.name}': usage '{texture.usage}' unknown.")
+
+                node.texture_properties.extra_flags = texture.extra_flags
+                ShaderCWXMLConverter.set_texture_usage_flags(node, texture)
+
+        if node.image is not None and not node.texture_properties.embedded:
+            # Set external texture name for non-embedded textures
+            node.image.source = "FILE"
+            node.image.filepath = "//" + param.texture_name + ".dds"
+
+    def assign_extra_detail_node(self):
+        """Assign extra detail node sampler image for viewing."""
+        dtl_ext = shader_nodes.get_detail_extra_sampler(self.bpy_object)
+        if dtl_ext:
+            dtl = self.bpy_object.node_tree.nodes["DetailSampler"]
+            dtl_ext.image = dtl.image
+
+    @staticmethod
+    def create_blank_image(name: str) -> bpy.types.Image:
+        """Create a blank 512x512 bpy image with the given name."""
+        return bpy.data.images.new(name=name, width=512, height=512)
+
+    @staticmethod
+    def set_value_node_properties(node: bpy.types.ShaderNodeValue, param: ydrxml.VectorShaderParameter):
+        axis = node.name[-1]
+        node.outputs[0].default_value = getattr(param, axis)
+
+    @staticmethod
+    def set_texture_usage_flags(node: bpy.types.ShaderNodeTexImage, texture: ydrxml.TextureItem):
+        """Set usage flags for embedded texture."""
+        for flag_bpy in dir(node.texture_flags):
+            for flag_cwxml in texture.usage_flags:
+                if flag_cwxml.lower() == flag_bpy:
+                    setattr(
+                        node.texture_flags, flag_bpy, True)
